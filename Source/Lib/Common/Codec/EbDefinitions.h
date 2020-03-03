108aa8b2e023f6d58565fe551542bb89539b3513
hguermaz <hassen.guermazi@intel.com>
2020-03-03 12:00:04 -0800

    Enhanced I_SICE QP assignment
    Signed-off-by: anaghdin <amir.naghdinezhad@intel.com>

--- Source/Lib/Common/Codec/EbDefinitions.h
+++ Source/Lib/Common/Codec/EbDefinitions.h
@@ -33,7 +33,7 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
+#define QPS_CHANGE_II            1 // Change the QP assignment for I
 #define CHROMA_SEARCH_OPT        1 // Move chroma search to be done on the best intra candidate survived from MD stage 2
 #if CHROMA_SEARCH_OPT
 #define INFR_OPT                 1 // Lossless: Infrastructure work to allow the protability of the chroma search
